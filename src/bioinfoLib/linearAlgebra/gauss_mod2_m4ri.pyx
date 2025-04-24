5ad44223f935fae9f76d1ab4457617c1748586ce
stanfish06 <zyyu@umich.edu>
2025-04-24 15:59:06 -0400

    add a faster gaussian elimination algorithm on mod2 based on the m4ri library

---
+++ src/bioinfoLib/linearAlgebra/gauss_mod2_m4ri.pyx
@@ -0,0 +1,37 @@
+cdef extern from "m4ri/m4ri.h":
+    ctypedef int rci_t
+    ctypedef int wi_t
+    ctypedef unsigned long long m4ri_word "word"
+    ctypedef int BIT
+    ctypedef struct mzd_t:
+        rci_t nrows
+        rci_t ncols
+        wi_t width
+
+    cdef mzd_t *mzd_init(rci_t, rci_t)
+    cdef void mzd_free(mzd_t *)
+    cdef void mzd_write_bit(mzd_t *m, rci_t row, rci_t col, BIT value)
+
+    cdef void mzd_print(mzd_t *)
+    cdef int mzd_solve_left(mzd_t *A, mzd_t *B, int cutoff, int inconsistency_check)
+
+
+def mod2Solve_m4ri_py(A, b):
+    nr = len(A)
+    nc = len(A[0])
+
+    cdef mzd_t *A2 = mzd_init(nr, nc);
+    cdef mzd_t *b2 = mzd_init(nr, 1);
+    
+    try:
+        for j in range(nc):
+            for i in range(nr):
+                mzd_write_bit(A2, i, j, A[i][j])
+                if j == 0:
+                    mzd_write_bit(b2, i, 0, b[i])
+        result = mzd_solve_left(A2, b2, 0, 1)
+        return result == 0
+    finally:
+        mzd_free(A2)
+        mzd_free(b2)
+
--- src/bioinfoLib/linearAlgebra/setup.py
+++ src/bioinfoLib/linearAlgebra/setup.py
@@ -1,7 +1,21 @@
 from Cython.Build import cythonize
-from setuptools import setup
+from setuptools import setup, Extension
+
+modules = [
+    Extension(
+        "gauss_mod2_m4ri",
+        sources = ["gauss_mod2_m4ri.pyx"],
+        libraries = ["m4ri"],
+        library_dirs = ["/usr/local/lib"],
+        include_dirs = ["/usr/local/include"]
+    ), 
+    Extension(
+        "gauss_mod2",
+        sources = ["gauss_mod2.pyx"]
+    )
+]
 
 setup(
-    name="gauss_mod2",
-    ext_modules=cythonize("gauss_mod2.pyx"),  # gdb_debug=True
+    name="gaussian_elimination",
+    ext_modules=cythonize(modules),  # gdb_debug=True
 )
