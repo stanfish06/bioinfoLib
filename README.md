f95d5c35403ecf07a42346bba6dc9cee36d73824
Stan <zyyu@umich.edu>
2025-06-16 18:36:01 -0400

    update dependencies to make it compatible on hpc

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ conda install nvidia::libcusparse conda-force::nccl nvidia::cuda-toolkit
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 ```
 For sksparse, it depends on openblas, and you need to make sure both libblas.so.3 and liblapack.so.3 point to the right version of openblas (if system openblas failed, try build from source and configure alternative)
+
+For juila, use 1.12.0-beta1+0.x64.linux.gnu if 1.11 failed due to curl issue.
--- src/bioinfoLib/julia/__init__.py
+++ src/bioinfoLib/julia/__init__.py
@@ -1,1 +1,18 @@
+import juliapkg
+
+packages = {
+    "Ripserer": ("aa79e827-bd0b-42a8-9f10-2b302677a641", None),
+    "StatsBase": ("2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91", None),
+    "Combinatorics": ("861a8166-3701-5b0c-9a16-15d98fcdc6aa", None),
+}
+for package, (uuid, version) in packages.items():
+    try:
+        if version:
+            juliapkg.add(package, uuid=uuid, version=version)
+        else:
+            juliapkg.add(package, uuid=uuid)
+    except Exception as e:
+        print(f"Error installing {package}: {e}")
+        raise e
+juliapkg.resolve()
 from . import setup, utils
