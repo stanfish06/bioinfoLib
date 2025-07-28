9cac3c74535ddf33481bd44218a7702c6b56ed69
Stan <zyyu@umich.edu>
2025-07-28 02:39:50 -0400

    add rpy2

---
+++ src/bioinfoLib/R/__init__.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+
+from dotenv import load_dotenv
+
+load_dotenv()
+r_ld_path = (
+    subprocess.check_output(["python", "-m", "rpy2.situation", "LD_LIBRARY_PATH"])
+    .decode("utf-8")
+    .strip()
+)
+existing = os.environ.get("LD_LIBRARY_PATH", "")
+os.environ["LD_LIBRARY_PATH"] = f"{r_ld_path}:{existing}"
+
+import rpy2.robjects as robjects
